f7d080771a3c8a762e406cd42248a73f8b6717b3
Srinu <42026561+Sreenivas2@users.noreply.github.com>
2018-08-24 00:30:26 +0530

    first commit

---
+++ js/all.js
@@ -0,0 +1,140 @@
+// alert("Welcome to javascript");
+//console.log("print console");
+// var a = 3;
+// console.log(a);
+// console.log(typeof(a));
+// var b = "Seven Hills Jaffa ";
+// console.log(b);
+
+// function test(){
+// 	var c = 3 ;
+// 	console.log(c);
+// };
+// test();
+// function demo1(){
+// 	var b = "hai";
+// 	console.log(b);
+// };
+// demo1();
+// console.log("b");
+// console.log(typeof(b));
+// var b = 3;
+// function first(){
+// 	console.log(b);
+// 	//console.log(a);
+// 	console.log("b");
+// 	//console.log(c);
+// 	console.log("c");
+// 	//console.log(a+b);
+// 	console.log("a+b");
+// };
+// first();
+
+// var c  = 6;
+// function demo2() {
+// 	var c  = 5;
+	
+// }
+// console.log(c);
+// demo2();
+// var x = 3;
+// var y = 4;
+// var z = (x+y);
+// console.log((x+y));
+// function demo3(){
+// 	var x = 3;
+// 	var y = 4;
+// 	var z = (x+y);
+// 	console.log((x+y));
+// };
+// demo3();
+// function demo4(){
+// 	var x = 6;
+// 	var x = 2;
+// 	console.log(x);
+// };
+// demo4();
+// var c = 6;
+// function demo5(){
+// 	var c = 5;
+// 	console.log(c);
+// };
+// demo5();
+// var c = 6;
+// function demo6(){
+// 	var c = 5;
+// };
+// demo6();
+// console.log(c);
+// function demo7(){
+// 	var a = 5;
+// 	var b = "6";
+// 	var c = 4;
+// 	var c = (b+a+c);
+// 	console.log(c);
+// };
+// demo7();
+// function demo8(){
+// 	var x = "2"+4+6+"3"+2+1;
+// 	console.log(x);
+// };
+// demo8();
+// function demo9(){
+// 	txt1 = "John";
+// 	txt2 = "Doe";
+// 	txt3 = txt1+" "+txt2;
+// 	console.log(txt3);
+// };
+// demo9();
+// function demo10(){
+// 	txt1 = "what a very";
+// 	txt1 += "nice day";
+// 	console.log(txt1);
+// };
+// demo10();
+// var a = 5;
+// var b = "5";
+// // console.log((a===b));
+// var a = [1,2,3,4,"welcome"];
+// console.log(typeof(a));
+// console.log(a[6]);
+// var cars = {name1:"maruthi",name2:"honda",name3:"fiat"};
+// console.log((cars.name3));
+// <p id= "test"></p>
+// var x = "ABCDEF";
+// var y = x.Length;
+// var y = 6;
+// document.getElementById("test").innerHTML = y;
+// var x = "John";
+// var y = new String("John");
+// console.log((x===y));
+// var b = {Name:"brahmaiah",age:"30"};
+// console.log(b.age);
+// var a = 3;
+// console.log(a);
+// console.log(a.toString());
+// var y = 16;
+// var z = y.toExponential(2);
+// console.log(z);
+// var x = 9.656;
+// var y = x.toFixed(2);
+// console.log(y);
+// var x = 9.656;
+// var y = x.toPrecision(2);
+// console.log(y);
+// var x = 123;
+// var y = x.valueOf();
+// console.log(y);
+// Number(true);
+Number(new Date(2017-09-30));
+console.log(Number(new Date(2017-09-30)));
+// console.log(Number(true));
+// parseInt("10");
+// console.log(parseInt("10"));
+// parseFloat("10");
+// console.log(parseFloat("10"));
+// var x = Number.MAX_VALUE;
+// console.log(x);
+// var x = 6;
+// var y = x.MAX_VALUE;
+// console.log(y);
